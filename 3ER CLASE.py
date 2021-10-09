d721b9cab33059a1263fb2cd99d2ee8bba5e9935
NezumiSora <cristina.negrete@alumnos.udg.mx>
2021-10-09 00:23:09 +0000

    3

---
+++ 3ER CLASE.py
@@ -0,0 +1,12 @@
+chars=['a','1','.','-']
+strings=["nombre", "abc", "sf023-.,","202"]
+nums=[1,2,3,3.4]
+booleanos=[True,False,True]
+combinados= [True, 1, 'a', 3, "asd"]
+
+nums=4
+exp=5
+resp=nums
+for i in range(exp):
+    print(resp)
+    resp*=nums
